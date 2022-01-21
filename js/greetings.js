90aa0add4d60634df6cc4b4bc7abd6df09f8324f
daram <dmdgi37@naver.com>
2022-01-21 21:43:59 +0900

    vanilaJS-clone-clock

--- js/greetings.js
+++ js/greetings.js
@@ -10,20 +10,20 @@ function onLoginSubmit(event) {
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
   localStorage.setItem(USERNAME_KEY, username);
-	paintGreetings(username);
+  paintGreetings(username);
 }
 
 function paintGreetings(username) {
- greeting.innerText= `Hello ${username}`;
+  greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
-if(savedUsername === null){
+if (savedUsername === null) {
   loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit",onLoginSubmit);
-}
-else {
-paintGreetings(savedUsername);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+
+  paintGreetings(savedUsername);
 
 }
